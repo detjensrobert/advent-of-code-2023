f5ec1fa0c37920eb72fd5aaa52bab6034e47256d
Robert Detjens <github@detjens.dev>
2023-12-03 22:36:03 -0800

    day3: part 1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,8 @@ use std::collections::HashSet;
 use std::env;
 use std::fs;
 
+use colored::Colorize;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() >= 2, "no input given!");
@@ -17,67 +19,44 @@ fn main() {
     part_two(&schematic);
 }
 
+type Point = (usize, usize);
+
 // What is the sum of part numbers next to symbols (king adjaceny)?
 fn part_one(schematic: &Vec<Vec<char>>) {
-    // find position of all engine symbols
-    // i.e. not digit, not .
-    // let chars: Vec<(usize, usize)> = input
-    //     .iter()
-    //     .enumerate()
-    //     .filter_map(|(i, row)| {
-    //         row.iter()
-    //             .enumerate()
-    //             .filter_map(|(j, sym)| {
-    //                 if !sym.is_digit(10) && *sym != '.' {
-    //                     Some(j)
-    //                 } else {
-    //                     None
-    //                 }
-    //             })
-    //     })
-    //     .collect()
-    //     .flatten();
+    let mut parts: Vec<Point> = Vec::new();
 
-    let mut parts: Vec<(i32, i32)> = Vec::new();
-
-    let row_max = schematic[0].len() as i32;
-    let col_max = schematic.len() as i32;
+    let row_max = schematic[0].len() as isize;
+    let col_max = schematic.len() as isize;
 
     // find all parts symbols
     for (i, row) in schematic.iter().enumerate() {
         for (j, sym) in row.iter().enumerate() {
             if !sym.is_digit(10) && *sym != '.' {
-                parts.push((i as i32, j as i32));
+                parts.push((i, j));
             }
         }
     }
 
     // find any adjacent numbers
-    let adjacents: [(i32, i32); 8] = [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
+    #[rustfmt::skip]
+    let adjacents: [(isize, isize); 8] = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1),
     ];
-    //
-    let mut digits: Vec<(i32, i32)> = Vec::new();
-    for (i, j) in parts {
-        // check surroundings around part
-        digits.extend(
+    let digits: Vec<Point> = parts
+        .iter()
+        .flat_map(|(i, j)| {
             adjacents
                 .iter()
-                .filter_map(|(a, b)| {
+                .filter_map(move |(a, b)| {
                     // filter out any OOB coords
-                    return match (i + a, j + b) {
+                    return match (*i as isize + a, *j as isize + b) {
                         (-1, _) => None,
                         (_, -1) => None,
                         (r, _) if r >= row_max => None,
                         (_, c) if c >= col_max => None,
-                        x @ (_, _) => Some(x),
+                        (x, y) => Some((x as usize, y as usize)),
                     };
                 })
                 // find any adjacent numbers
@@ -88,21 +67,57 @@ fn part_one(schematic: &Vec<Vec<char>>) {
                         None
                     }
                 })
-        );
+        })
+        .collect();
+
+    // find start and length of number for each point found
+    // use Set for deduplication
+    let mut starts: HashSet<Point> = HashSet::new();
+    // make string rows from array of chars
+    let rows: Vec<String> = schematic
+        .iter()
+        .map(|r| r.iter().collect::<String>())
+        .collect();
+    for (i, j) in digits {
+        let row = &rows[i as usize];
+        // char after first non-digit, searching back from original digit j
+        let start = match row[..j].rfind(|c: char| !c.is_digit(10)) {
+            Some(x) => x + 1, // char after the found non-digit
+            None => 0,        // or the start of string if no non-digits found
+        };
+
+        starts.insert((i, start));
     }
 
-    let mut starts: HashSet<(i32, i32)> = HashSet::new();
+    // // highlight found digits
+    // for (i, row) in schematic.iter().enumerate() {
+    //     for (j, sym) in row.iter().enumerate() {
+    //         if starts.contains(&(i, j)) {
+    //             print!("{}", String::from(*sym).red())
+    //         } else {
+    //             print!("{sym}")
+    //         }
+    //     }
+    //     println!("")
+    // }
 
+    // now parse those digits
+    // (we need to wait until after all are found before parsing because there
+    // might be duplicate digits in the found list)
+    let part_numbers = starts.iter().map(|(row, start)| {
+        schematic[*row][*start..]
+            .iter()
+            .take_while(|c| c.is_digit(10))
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap()
+    });
 
-
-    println!("found digits at:");
-    for (r, c) in digits {
-        println!("({r},{c}): '{}'", schematic[r as usize][c as usize]);
-    }
-
-    println!("Part 1: {}", 0);
+    println!("Part 1: {}", part_numbers.sum::<usize>());
 }
 
-fn part_two(input: &Vec<Vec<char>>) {
+
+fn part_two(schematic: &Vec<Vec<char>>) {
+
     println!("Part 2: {}", 0);
 }
